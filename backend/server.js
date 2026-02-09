a5683bf69b3538e5c9af002ae98dd47da6ec33a9
shoryataneja <shoryataneja5@gmail.com>
2026-02-10 03:21:13 +0530

    Fixed the issue of cors and connected frontend with backend and store JWT

--- backend/server.js
+++ backend/server.js
@@ -1,37 +1,29 @@
-const express = require('express');
-const authRoutes = require("./routes/authRoutes")
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
 const tripRoutes = require("./routes/tripRoutes");
 const expenseRoutes = require("./routes/expenseRoutes");
 
-require("dotenv").config();
-const connectDB = require("./config/db");
-
-
-
-
 const app = express();
 
-//middleware
+// ✅ CORS must come BEFORE routes
+app.use(cors());
 app.use(express.json());
-
 
 connectDB();
 
-
-//routes
+// routes
 app.use("/api/auth", authRoutes);
 app.use("/api/trips", tripRoutes);
 app.use("/api/expenses", expenseRoutes);
 
-
-
-
-
-app.get("/",(req,res)=>{
-    res.send("SplitEase backend is running")
+app.get("/", (req, res) => {
+  res.send("SplitEase backend is running");
 });
 
-app.listen(3000,()=>{
-    console.log("Server running on port 3000");
-})
-
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
