2f3b78a28f0e98e4b751a70be5d3b9039e05e67f
shoryataneja <shoryataneja5@gmail.com>
2026-02-11 17:36:30 +0530

    Fetch and displaying trips on the dashboard page

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,5 +1,40 @@
+import { useEffect, useState } from "react";
+import API from "../services/api";
+
 function Dashboard() {
-  return <h2>Dashboard</h2>;
+  const [trips, setTrips] = useState([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const fetchTrips = async () => {
+      try {
+        const res = await API.get("/trips");
+        setTrips(res.data.trips);
+      } catch (err) {
+        setError("Failed to load trips");
+      }
+    };
+
+    fetchTrips();
+  }, []);
+
+  return (
+    <div>
+      <h2>Dashboard</h2>
+
+      {error && <p>{error}</p>}
+
+      {trips.length === 0 ? (
+        <p>No trips found</p>
+      ) : (
+        <ul>
+          {trips.map((trip) => (
+            <li key={trip._id}>{trip.name}</li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 }
 
 export default Dashboard;
