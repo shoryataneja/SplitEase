5961fea43b384fe05c32f8497492024fc5428fc6
shoryataneja <shoryataneja5@gmail.com>
2026-02-12 22:50:54 +0530

    Fixed a routing bug

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,6 +13,7 @@ function App() {
   <Route path="/" element={<Navigate to="/login" />} />
 
   <Route path="/login" element={<Login />} />
+  
   <Route path="/signup" element={<Signup />} />
 
   <Route
